215ce6177edb60116324aa35e66db6b4c41284d0
André Voronhuk <andrevoronhuk@gmail.com>
2022-02-03 12:11:33 -0300

    Resolvido bug entre index 1 e 11
    adição de [ e ] entre os numeros da string que armazena os TODOs completos

--- todo.js
+++ todo.js
@@ -8,12 +8,11 @@ var completedTodos = localStorage.getItem('completed') || [];
 renderTodos(); //primeira Render da pagina
 function renderTodos() {
     listElement.innerHTML = '';
- 
+    pos = 0
     for (var todo of todos) {
         var todoElement = document.createElement('li');
         inputElement.value = '';
         if (todo != '' && todo != ' ') {
-            var pos = todos.indexOf(todo);
             todoElement.id = "todo " + pos
             var linkElement = document.createElement('a');
             linkElement.setAttribute('href', '#');
@@ -29,7 +28,7 @@ function renderTodos() {
             linkElement.style.textDecoration = 'none';
             completeElement.setAttribute("id", pos)
             completeElement.setAttribute('onclick', 'complete(' + pos + ')')
-            if (completedTodos.indexOf(pos) >= 0) {
+            if (completedTodos.indexOf(`[${pos}]`) >= 0) {
                 completeElement.setAttribute("checked", true)
                 okElement.className = "CheckedTodo"
             } else {
@@ -41,16 +40,19 @@ function renderTodos() {
             todoElement.appendChild(okElement);
             todoElement.appendChild(completeElement);
             listElement.appendChild(todoElement);
+            pos = pos + 1
         }
     }
 } //renderiza os afazeres
 
 function complete(pos) {
-    if (completedTodos.indexOf(pos) >= 0) {
-        completedTodos = completedTodos.replace(pos, '')
+    if (completedTodos.indexOf(`[${pos}]`) >= 0) {
+        completedTodos = completedTodos.replace(`[${pos}]`, '')
+        completedTodos = completedTodos.replace("[]", "")
         localStorage.setItem('completed', completedTodos)
     } else {
-        completedTodos = completedTodos + '' + pos
+        completedTodos = completedTodos + `[${pos}]`
+
         localStorage.setItem('completed', completedTodos)
     }
     renderTodos();
@@ -80,20 +82,22 @@ function addTodo() {
 } //adiciona uma tarefa a ser feita
 function deleteTodo(pos) {
     todos.splice(pos, 1);
-    if (completedTodos.indexOf(pos) >= 0) {
-        completedTodos = completedTodos.replace(pos, "")
-        localStorage.setItem('completed', completedTodos)
+    if (completedTodos.indexOf(`[${pos}]`) >= 0) {
+        completedTodos = completedTodos.replace(`[${pos}]`, "")
+        completedTodos = completedTodos.replace("[]", "")
+    };
+    localStorage.setItem('completed', completedTodos)
 
-        for (let index = 0; index <= completedTodos.length; index++) {
-            numero = completedTodos[index]
-            if (numero == '' || numero == " ") {} else {
-                if (numero > pos) {
-                    completedTodos = completedTodos.replace(numero, numero - 1)
-                }
-            }
+    for (let index = 0; index <= completedTodos.length; index++) {
+        numero = completedTodos[index]
+
+        if (numero > pos) {
+            completedTodos = completedTodos.replace(`[${numero}]`, `[${numero-1}]`)
         }
-        localStorage.setItem('completed', completedTodos)
     }
+
+    localStorage.setItem('completed', completedTodos)
+
     renderTodos();
     saveStorage()
 } //Deleta o elemento com base na posicao dele no Array 'todos'
